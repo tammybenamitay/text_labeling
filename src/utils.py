204df5e7d34be870364df2b77e6da795b8049c30
tammy <btammy@gmail.com>
2023-10-04 17:36:08 +0300

    add utils and clustering

---
+++ src/utils.py
@@ -0,0 +1,88 @@
+import os
+import pickle
+import openai
+import config
+import nltk
+import utils
+
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+from nltk.corpus import stopwords
+
+stop_words = stopwords.words('english')
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
+from gensim.summarization import summarize
+
+prt = nltk.stem.PorterStemmer()
+nltk.download('punkt')
+#os.system('python -m pip install requirements.txt')
+
+def printaa():
+    print("This is a test function in utils.py")
+
+def preprocess(document_path):
+    with open(document_path, 'r', encoding='utf-8') as file:
+        document = file.read()
+        tokens = nltk.word_tokenize(document)
+        tokens_pun_lower = [i.lower() for i in tokens if i.isalnum()]
+        tokens_stop = [i for i in tokens_pun_lower if i not in stop_words]
+        terms = [prt.stem(i) for i in tokens_stop]
+        return " ".join(terms)
+    
+def load_data(data_folder_path, word_count_num):
+    data_list = []
+    #summarize_data = []
+    folder_names = []
+
+    for folder_name in os.listdir(data_folder_path):
+        folder_path = os.path.join(data_folder_path, folder_name)
+        
+        if os.path.isdir(folder_path):
+            folder_names.extend([folder_name] * len(os.listdir(folder_path)))  
+            
+            for file_name in os.listdir(folder_path):
+                file_path = folder_path+'/'+file_name
+                file_data = utils.preprocess(file_path)
+                data_list.append(file_data)
+                #summarize_data.append(summarize(file_data, word_count=word_count_num))
+
+    df = pd.DataFrame({'label': folder_names, 'text': data_list})
+    return df
+
+def create_model_LogisticRegression(encoded_texts,max_iter_num ):
+    X_train, X_test, y_train, y_test = train_test_split(encoded_texts, df['label'], test_size=0.2, random_state=42)
+
+    clf = LogisticRegression(max_iter=max_iter_num)
+    clf.fit(X_train, y_train)
+
+    y_pred = clf.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    report = classification_report(y_test, y_pred)
+
+    print(f"Accuracy: {accuracy}")
+    print("Classification Report:")
+    print(report)
+
+    return clf
+
+
+def generate_text_with_openai(subject):
+
+    api_key = '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'
+
+    which_suject = "Write a 150 words article about"+subject
+
+    # Configure GPT-3 parameters
+    response = openai.Completion.create(
+        engine="text-davinci-001",  # You can choose other engines like 'text-davinci-003'
+        prompt=which_suject,
+        max_tokens=150,  # Adjust the desired length of the generated text
+        api_key=api_key
+    )
+
+    # Extract the generated text from the response
+    print(response.choices)
+    generated_text = response.choices[0].text
+    return generated_text
